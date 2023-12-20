13f53a117040ecc10403468769d1d60697cc781d
DharmeshV05 <90438519+DharmeshV05@users.noreply.github.com>
2023-12-20 13:30:36 +0530

    Update Header.jsx
    navbar update changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav>
       <h1>WEb.Dev</h1>
-      <main>
-        <HashLink to={"/#home"}>Home</HashLink>
-        <Link to={"/contact"}>Contact</Link>
-        <HashLink to={"/#about"}>About</HashLink>
-        <HashLink to={"/#brands"}>Brands</HashLink>
-        <Link to={"/services"}>Services</Link>
+      <div className={`menu-toggle ${isMenuOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <main className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+        <HashLink to={"/#home"} onClick={toggleMenu}>
+          Home
+        </HashLink>
+        <Link to={"/contact"} onClick={toggleMenu}>
+          Contact
+        </Link>
+        <HashLink to={"/#about"} onClick={toggleMenu}>
+          About
+        </HashLink>
+        <HashLink to={"/#brands"} onClick={toggleMenu}>
+          Brands
+        </HashLink>
+        <Link to={"/services"} onClick={toggleMenu}>
+          Services
+        </Link>
       </main>
     </nav>
   );
